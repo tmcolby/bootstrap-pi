e6421594aaf03639bbc5a171fd1f0086b5c0824a
tyson <tyson.colby@innotech.engineering>
2018-02-01 16:09:11 -0800

    install packages and git repo

--- setup-new-node.sh
+++ setup-new-node.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 ansible-playbook create-new-user.yml -i hosts -c paramiko
 ansible-playbook secure-pi-account.yml -i hosts_secure_pi 
-
+ansible-playbook init-pi.yml -i hosts_secure_pi 
+sleep 60
+ansible-playbook install-packages.yml -i hosts_git
+ansible-playbook git.yml -i hosts_git
+#ping blackberrypi.local -w 60
